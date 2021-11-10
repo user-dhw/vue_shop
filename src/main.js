2c8241b6edad9998a9715b82985dc2d6b4d794f6
dhw <9681552+dinghw@user.noreply.gitee.com>
2021-11-10 21:23:13 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./plugins/element.js";
+//引入全局样式表
+import "./assets/css/global.css";
 
-Vue.config.productionTip = false
+// 引入axios包
+import axios from "axios";
+//配置请求根路径
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
+// 挂载到vue原型上
+Vue.prototype.$http = axios;
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: (h) => h(App),
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../components/Login";
+import Home from "../components/Home";
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes
-})
+    routes: [
+        { path: "/", redirect: "/login" },
+        { path: "/login", component: Login },
+        { path: "/Home", component: Home },
+    ],
+});
+//挂载路由导航首守卫
+router.beforeEach((to, from, next) => {
+    // to访问路径
+    //from从哪个路径跳转过来
+    //next 是一个函数,表示放行
+    // next() 放行  next('/login')强制跳转路径
 
-export default router
+    if (to.path === "/login") return next();
+    // 获取token
+    const tokenStr = window.sessionStorage.getItem("token");
+    if (!tokenStr) return next("/login");
+    next();
+});
+export default router;
